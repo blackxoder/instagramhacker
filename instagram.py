a5eb516b5521ff5e824e9dffc86e1fef5952bf8e
blackxoder <68056718+blackxoder@users.noreply.github.com>
2020-07-09 12:45:32 +0300

    Add files via upload
    instagram hesapları çalmak içim

---
+++ instagram.py
@@ -0,0 +1,14 @@
+import os
+os.system("clear")
+
+banner="""
+----------                                                       --------
+          ------------                                   --------
+                       --------------        ------------
+                                      ------
+"""
+print(banner)                                     
+
+isim = input('instagramAdı : ')
+soyisim = input('e-Mail : ')
+print(isim+"adlı hesabın sıfırlama bilgileri  "+soyisim+"adlı mail adresine gönderiliyor.")
